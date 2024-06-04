a13267ed0655e67a9775e98d8c2247001cfce962
s-moloo <shiraz.moloo@edu.sait.ca>
2024-06-04 10:13:47 -0600

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,27 @@
+
+
+import StudentInfo from './student-info';
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <StudentInfo/>
+      </main>
+    );
+  }
+
+
+
+// const Page1 = () => {
+//   return (
+//     <div>
+//       <h1>Week 2 Page</h1>
+//       <StudentInfo />
+//     </div>
+//   );
+// };
+
+//export default Page;
+
+
---
+++ app/week-2/student-info.js
@@ -0,0 +1,18 @@
+
+
+//import React from 'react';
+
+export default function StudentInfo() {
+
+//const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Shiraz Moloo</h1>
+      {/* <a href="https://github.com/s-moloo" target="_blank" rel="noopener noreferrer"> */}
+        {/* My GitHub Repository */}
+        <h2>"https://github.com/s-moloo" </h2>
+    </div>
+  );
+}
+//}
+
